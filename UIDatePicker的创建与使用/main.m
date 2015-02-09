ca2bef247fb891828f6cc2685236c3b9dffcf9c6
cjq002 <chenjiaqing@Mac-mini-CJQ.local>
2015-02-09 08:34:09 +0800

    Initial Commit

---
+++ UIDatePicker的创建与使用/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIDatePicker的创建与使用
+//
+//  Created by 陈家庆 on 15-2-9.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ UIDatePicker的创建与使用Tests/UIDatePicker______Tests.m
@@ -0,0 +1,34 @@
+//
+//  UIDatePicker______Tests.m
+//  UIDatePicker的创建与使用Tests
+//
+//  Created by 陈家庆 on 15-2-9.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIDatePicker______Tests : XCTestCase
+
+@end
+
+@implementation UIDatePicker______Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
